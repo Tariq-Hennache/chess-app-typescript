229e56fadd09c4d7050ececf10f67c667ca19c7d
44771 <tariq.hennache@gmail.com>
2023-03-29 18:42:37 +0100

    chessboard grid made with no styling

---
+++ src/components/Chessboard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Chessboard.css';
+
+const horizontalAxis = ["a", "b", "c", "d", "e", "f","g", "h"];
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8] ;
+function Chessboard(){
+    let board = []
+    for(let j=verticalAxis.length -1; j>=0 ; j--){
+    for(let i=0; i< horizontalAxis.length; i++){
+            board.push(<div className="tile">[{horizontalAxis[i]} {verticalAxis[j]}]</div>)
+        }
+    }
+
+    return <div id="chessboard">{board}</div>
+};
+
+export default Chessboard;
