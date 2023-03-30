3d1c6a4ce457a15cad9e97e3af6ad55ff9b59d25
44771 <tariq.hennache@gmail.com>
2023-03-30 17:46:06 +0100

    all pieces now on board

--- src/components/chessboard/Chessboard.tsx
+++ src/components/chessboard/Chessboard.tsx
@@ -19,6 +19,25 @@ for( let i =0; i< 8; i++){
     pieces.push({image : 'assets/images/Chess_plt60.png', x:i, y:1})
 }
 
+pieces.push({image : 'assets/images/Chess_rdt60.png', x:0, y:7})
+pieces.push({image : 'assets/images/Chess_rdt60.png', x:7, y:7})
+pieces.push({image : 'assets/images/Chess_rlt60.png', x:0, y:0})
+pieces.push({image : 'assets/images/Chess_rlt60.png', x:7, y:0})
+pieces.push({image : 'assets/images/Chess_ndt60.png', x:1, y:7})
+pieces.push({image : 'assets/images/Chess_ndt60.png', x:6, y:7})
+pieces.push({image : 'assets/images/Chess_nlt60.png', x:1, y:0})
+pieces.push({image : 'assets/images/Chess_nlt60.png', x:6, y:0})
+pieces.push({image : 'assets/images/Chess_bdt60.png', x:2, y:7})
+pieces.push({image : 'assets/images/Chess_bdt60.png', x:5, y:7})
+pieces.push({image : 'assets/images/Chess_blt60.png', x:2, y:0})
+pieces.push({image : 'assets/images/Chess_blt60.png', x:5, y:0})
+
+pieces.push({image : 'assets/images/Chess_qdt60.png', x:3, y:7})
+pieces.push({image : 'assets/images/Chess_qlt60.png', x:3, y:0})
+pieces.push({image : 'assets/images/Chess_kdt60.png', x:4, y:7})
+pieces.push({image : 'assets/images/Chess_klt60.png', x:4, y:0})
+
+
 function Chessboard(){
     let board = []
     for(let j=verticalAxis.length -1; j>=0 ; j--){
