f53b129a1ae09c6a026ac2bc09711305a509490e
44771 <tariq.hennache@gmail.com>
2023-04-05 00:47:27 +0100

    added logic for moving a piece

--- src/components/chessboard/Chessboard.tsx
+++ src/components/chessboard/Chessboard.tsx
@@ -34,6 +34,8 @@ for( let i =0; i< 8; i++){
     pieces.push({image : 'assets/images/Chess_plt60.png', x:i, y:1})
 }
 
+let activePiece: HTMLElement | null = null;
+
 function grabPiece(e: React.MouseEvent){
     const element = e.target as HTMLElement
 
@@ -45,12 +47,21 @@ function grabPiece(e: React.MouseEvent){
         element.style.left = `${x}px`
         element.style.top = `${y}px`
 
-
+        activePiece = element
     }
 }
 
 function movePiece(e : React.MouseEvent){
+    if(activePiece){
 
+        const x = e.clientX -50;
+        const y = e.clientY -50;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`
+        activePiece.style.top = `${y}px`
+
+
+    }
 }
 
 
