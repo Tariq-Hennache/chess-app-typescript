59fb80d1c9a9acc96bdf2a1d81a61099e1b055d6
44771 <tariq.hennache@gmail.com>
2023-05-30 20:00:29 +0100

    bishop movement logic

--- src/referee/Referee.tsx
+++ src/referee/Referee.tsx
@@ -40,6 +40,88 @@ class Referee{
 
     }
 
+    // isPieceInTheWay(px:number, py:number, x:number, y:number, boardState:Piece[], type:PieceType){
+    //     if(type === PieceType.PAWN){
+    //         const direction = y - py > 0 ? 1 : -1;
+    //         if(this.isTileOccupied(x, y, boardState)){
+    //             return true;
+    //         }else if(this.isTileOccupied(x, y - direction, boardState)){
+    //             return true;
+    //         }
+    //     }else if(type === PieceType.KNIGHT){
+    //         return false;
+    //     }else if(type === PieceType.BISHOP){
+    //         const xDirection = x - px > 0 ? 1 : -1;
+    //         const yDirection = y - py > 0 ? 1 : -1;
+    //         let x1 = px + xDirection;
+    //         let y1 = py + yDirection;
+    //         while(x1 !== x && y1 !== y){
+    //             if(this.isTileOccupied(x1, y1, boardState)){
+    //                 return true;
+    //             }
+    //             x1 += xDirection;
+    //             y1 += yDirection;
+    //         }
+    //     }else if(type === PieceType.ROOK){
+    //         const xDirection = x - px > 0 ? 1 : -1;
+    //         const yDirection = y - py > 0 ? 1 : -1;
+    //         if(x === px){
+    //             let y1 = py + yDirection;
+    //             while(y1 !== y){
+    //                 if(this.isTileOccupied(x, y1, boardState)){
+    //                     return true;
+    //                 }
+    //                 y1 += yDirection;
+    //             }
+    //         }else if(y === py){
+    //             let x1 = px + xDirection;
+    //             while(x1 !== x){
+    //                 if(this.isTileOccupied(x1, y, boardState)){
+    //                     return true;
+    //                 }
+    //                 x1 += xDirection;
+    //             }
+    //         }
+    //     }else if(type === PieceType.QUEEN){
+    //         const xDirection = x - px > 0 ? 1 : -1;
+    //         const yDirection = y - py > 0 ? 1 : -1;
+    //         if(x === px){
+    //             let y1 = py + yDirection;
+    //             while(y1 !== y){
+    //                 if(this.isTileOccupied(x, y1, boardState)){
+    //                     return true;
+    //                 }
+    //                 y1 += yDirection;
+    //             }
+    //         }else if(y === py){
+    //             let x1 = px + xDirection;
+    //             while(x1 !== x){
+    //                 if(this.isTileOccupied(x1, y, boardState)){
+    //                     return true;
+    //                 }
+    //                 x1 += xDirection;
+    //             }
+    //         }else if(Math.abs(x - px) === Math.abs(y - py)){
+    //             let x1 = px + xDirection;
+    //             let y1 = py + yDirection;
+    //             while(x1 !== x && y1 !== y){
+    //                 if(this.isTileOccupied(x1, y1, boardState)){
+    //                     return true;
+    //                 }
+    //                 x1 += xDirection;
+    //                 y1 += yDirection;
+    //             }
+    //         }
+    //     }else if(type === PieceType.KING){
+    //         if(Math.abs(x - px) <= 1 && Math.abs(y - py) <= 1){
+    //             return false;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+
     
 
     isValidMove(px:number, py:number, x:number, y:number, type:PieceType, team:Team, boardState:Piece[]){
@@ -75,6 +157,17 @@ class Referee{
                     console.log("knight")
                 }
             }
+
+            else if(type === PieceType.BISHOP){
+                if(Math.abs(x - px) === Math.abs(y - py)){
+                    if(!this.isTileOccupied(x, y, boardState)){
+                        return true;
+                    }else if(this.tileHasEnemyPiece(x, y, team, boardState)){
+                        return true;
+                    }
+                }
+            }
+
         
 
         
